6f663388e0f0f7295567e2a86b2dfc428ff7e2cc
tianfudhe <tianfu.d.he@gmail.com>
2016-05-14 13:25:41 +0800

    sync

--- pelicanconf.py
+++ pelicanconf.py
@@ -34,7 +34,7 @@ DEFAULT_PAGINATION = 8
 PLUGIN_PATHS = ["./plugins"]
 PLUGINS = ["render_math"]
 
-THEME = './pelican-themes/aboutwilson'
+THEME = "pelican-themes/crowsfoot"
 
 DISQUS_SITENAME = u'tianfudhe'
 
